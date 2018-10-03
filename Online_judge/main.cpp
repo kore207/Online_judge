9ebb213cd552834962ce5a27d7e0313d7c0e6339
kim ggun <kore207@naver.com>
2018-10-03 11:28:34 +0900

    Initial Commit

---
+++ Online_judge/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Online_judge
+//
+//  Created by Kim GT  on 03/10/2018.
+//  Copyright © 2018 Kim GT . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
