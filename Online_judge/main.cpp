e0531bbf5608c81f18ddc6a4ece38f1f17bcbcdf
kim ggun <kore207@naver.com>
2018-10-08 08:55:27 +0900

    Online Judge 4344 [If syntax]

--- Online_judge/main.cpp
+++ Online_judge/main.cpp
@@ -11,27 +11,36 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    int score_num;
-    float score[1000] ;
-    int score_max=1 ;
-    float score_sum = 0 ;
-    cin >> score_num ;
-    for(int i = 0 ; i < score_num ; i++)
+    int std_num;
+    int std_score[1000] ;
+    float std_sum = 0;
+    float std_avg ;
+    int avg_up = 0;
+    float avg_ratio = 0 ;
+    int part_num ;
+    cin >> part_num ;
+    cout << fixed ;
+    cout.precision(3) ;
+    for(int i = 0; i< part_num; i++)
     {
-        cin >> score[i] ;
-        if(score[i] > score_max)
+        cin >> std_num ;
+        for(int j = 0; j < std_num ; j ++)
         {
-            score_max = score[i] ;
+            cin >> std_score[j] ;
+            std_sum += std_score[j] ;
         }
-    }
-    for(int i=0 ; i <score_num; i++)
-    {
+        std_avg = std_sum / std_num ;
+        for(int k = 0 ; k <std_num ; k++)
+        {
+            if(std_score[k] > std_avg)
+                avg_up++ ;
+        }
+        avg_ratio = (float)avg_up / (float)std_num ;
+        cout << avg_ratio*100 <<"%"<<endl ;
+//        std_avg = 0;
+        avg_up = 0 ;
+        std_sum = 0 ;
         
-        score[i] = (score[i]/score_max)*100 ;        
-        score_sum += score[i];
     }
-    float avg = score_sum/score_num ;
-    cout << avg ;
-    
-    return 1;
+    return 0 ;
 }
