1f7718ec533df505bb7bb198a2434953e2f9021d
gt <kore17641764@gmail.com>
2019-01-09 21:58:02 +0900

    백준 알고리즘 #11284980
    단계별 문제 풀어보기 JAVA INPUT

--- OJ2557/src/helloworld/hello.java
+++ OJ2557/src/helloworld/hello.java
@@ -1,9 +1,14 @@
 package helloworld;
 
+import java.util.Scanner;
 public class hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(sc.nextInt() + sc.nextInt());
+
 	}
 
 }
