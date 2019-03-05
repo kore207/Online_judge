516e7002e9fe2e9620ff26111062620d955ab8b8
gt <kore207@naver.com>
2019-03-05 22:58:17 +0900

    단계 7 풀이

---
+++ OJ11654/src/OJ11654/Main.java
@@ -0,0 +1,18 @@
+package OJ11654;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+ 		char ch = sc.next().charAt(0);
+ 		
+		System.out.print((int)ch);
+		sc.close();
+		
+		
+	}
+
+}
